188ef253151e79548c2cae1e5a4e451f3bdd42f4
Gabriele <124803038+SirPtavius@users.noreply.github.com>
2024-03-20 16:49:11 +0100

    fixed the creation of square and the size adaptation of cells

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+const GRIDSIDE = 600
 let container = document.getElementById("container")
-for(let i = 0; i < 256; i++){
+container.style.width = container.style.height = `${GRIDSIDE}px`;
+
+
+let buttonCreateSquare = document.querySelector("#createSquare")
+buttonCreateSquare.addEventListener("click", () => {
+    
+    let grid = document.getElementById("squares").value
+    
+    if(grid < 16 || grid > 100){
+        grid = 16
+    } 
+    
+    container.innerHTML ="";
+    createSquare(grid)
+});
+
+
+function createSquare(side){
+    
+    for(let i = 0; i < (side*side); i++){
         let cell = document.createElement("div");
+        cell.style.width = cell.style.height = `${(GRIDSIDE / side) - 2}px`;
         cell.classList.add("cell");
-        //cell.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
         container.appendChild(cell);
+
+        // Aggiungi event listener per il passaggio del mouse
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = '#ff0000'; // Cambia il colore al passaggio del mouse
+        });
+
+}
+        
 }
